96611c778902f458282155474405aef4449f3d7f
jheddings <jason@heddings.com>
2017-06-12 20:50:49 -0600

    Initisl commands for getting market data

--- bin/bittrex.pl
+++ bin/bittrex.pl
@@ -24,6 +24,8 @@ use Config::Simple;
 
 =head1 OPTIONS
 
+=head2 Key / Secret
+
 =over
 
 =item B<--keyfile=file> : Load API key and secret from the specified file
@@ -31,6 +33,18 @@ use Config::Simple;
 =item B<--apikey=str> : Specify the user's API key
 
 =item B<--secret=str> : Provide the secret for API authentication
+
+=back
+
+=head2 Public Methods
+
+=item B<--getmarkets> : get the open and available trading markets at Bittrex
+
+=item B<--getcurrencies> : get all supported currencies at Bittrex
+
+=item B<--getticker=mkt> : get the current tick values for a market (e.g. BTC-LTC)
+
+=over
 
 =back
 
@@ -44,10 +58,17 @@ my $keyfile = undef;
 my $apikey = undef;
 my $secret = undef;
 
+my $do_getmarkets = undef;
+my $do_getcurrencies = undef;
+my $do_getticker = undef;
+
 GetOptions(
   'keyfile=s' => \$keyfile,
   'apikey=s' => \$apikey,
   'secret=s' => \$secret,
+  'getmarkets' => \$do_getmarkets,
+  'getcurrencies' => \$do_getcurrencies,
+  'getticker=s' => \$do_getticker,
   'help|?' => \$opt_help
 ) or usage(1);
 
@@ -55,7 +76,7 @@ usage(0) if $opt_help;
 
 usage('Must provide keyfile or apikey') unless ((defined $keyfile) or (defined $apikey));
 usage('Provide either keyfile or apikey (but not both)') if ((defined $keyfile) and (defined $apikey));
-usage('Must provide secret for apikey') unless ((defined $secret) and (defined $apikey));
+usage('Must provide secret for apikey') if ((not defined $secret) and (defined $apikey));
 usage("Key file not found: $keyfile") if ((defined $keyfile) && (not -f $keyfile));
 
 ################################################################################
@@ -97,4 +118,48 @@ if (defined $keyfile) {
 
 # common client instance
 my $bittrex = Bittrex->new($apikey, $secret);
+
+#---------------------------------------
+# /public/getmarkets
+if ($do_getmarkets) {
+  my $markets = $bittrex->getmarkets();
+  if ($markets) {
+    foreach (@{ $markets }) {
+      printf("%s : %sActive\n", $_->{MarketName}, ($_->{IsActive} ? '' : 'Not '));
+    }
+  } else {
+    printf("No market data available\n");
+  }
+}
+
+#---------------------------------------
+# /public/getcurrencies
+if ($do_getcurrencies) {
+  my $currencies = $bittrex->getcurrencies();
+  if ($currencies) {
+    foreach (@{ $currencies }) {
+      printf("%s [%s] : %sActive\n", $_->{CurrencyLong}, $_->{Currency},
+             ($_->{IsActive} ? '' : 'Not '));
+    }
+  } else {
+    printf("No market data available\n");
+  }
+}
+
+#---------------------------------------
+# /public/getticker
+if ($do_getticker) {
+  my $ticker = $bittrex->getticker($do_getticker);
+  if ($ticker) {
+      my $bid = $ticker->{Bid};
+      my $ask = $ticker->{Ask};
+      my $last = $ticker->{Last};
+      my $spread = ($ask - $bid) / $ask;
+
+      printf("%f / %f (%.4f%%) : %f\n", $bid, $ask, 100*$spread, $last)
+
+  } else {
+    printf("Ticker data unavailable for %s\n", $do_getticker);
+  }
+}
 
